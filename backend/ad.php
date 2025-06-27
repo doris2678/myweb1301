c798dea74f729f5db58c87d42a1233e669cba50c
doris <dorishsh2678@gmail.com>
2025-06-27 13:04:42 +0800

    新增mvim

--- backend/ad.php
+++ backend/ad.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">動態文字廣告管理</p>
-    <form method="post"  action="./api/edit_ad.php">
+    <form method="post"  action="./api/edit.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -9,7 +9,7 @@
                     <td width="10%">刪除</td>
                 </tr>
                 <?php
-                 $rows=$Ad->all();
+                 $rows=${ucfirst($do)}->all();
                  foreach($rows as $row):
                 ?>
                 <tr>
@@ -33,8 +33,9 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
+                    <input type="hidden" name="table" value="<?=$do;?>">
                     <td width="200px"><input type="button"
-                            onclick="op('#cover','#cvr','./modal/ad.php')"
+                            onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')"
                             value="新增動態文字廣告"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input
                             type="reset" value="重置"></td>
--- modal/mvim.php
+++ modal/mvim.php
@@ -1,13 +1,9 @@
-<h3 style='text-align:center;'>新增標題區圖片</h3>
+<h3 style='text-align:center;'>新增動畫圖片</h3>
 <hr>
 <form action="./api/insert.php" method='post' enctype="multipart/form-data">
     <div>
-        <label>標題區圖片：</label>
+        <label>動畫圖片：</label>
         <input type="file" name="img">
-    </div>
-    <div>
-        <label>標題區替代文字：</label>
-        <input type="text" name="text">
     </div>
     <div>
         <input type="hidden" name="table" value="<?=$_GET['table'];?>">
